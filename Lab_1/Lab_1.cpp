64e8554bcfd062c60dcaa2720056aff9cdf08044
Саша <epra11111@mail.ru>
2023-10-10 22:36:04 +0400

    Добавьте файлы проекта.

---
+++ Lab_1/Lab_1.cpp
@@ -0,0 +1,13 @@
+﻿#include "Lab_1.h"
+#include<iostream>
+using namespace std;
+using namespace matrix;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	Matrix<int> a(5, 3, 5);
+	Matrix<int> b(4, 5, 1);
+	Matrix<int> c(4, 5, 1);
+	auto result = a * b;
+	cout << result;
+}
